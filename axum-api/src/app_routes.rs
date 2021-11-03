6f0550a240c62d74b5e707e0bb88d7fd970e508c
Simon Osaka <geniusmickymouse@qq.com>
2021-11-03 11:49:22 +0800

    upgrade axum

--- axum-api/src/app_routes.rs
+++ axum-api/src/app_routes.rs
@@ -1,32 +1,42 @@
 use crate::{
-    adventures::get::get_adventure, app_index::index, change_password::change_password,
-    change_username::change_username, list::list_adventures, login::login, me::me,
-    play_list::play_list_adventures, registry::registry, sync::sync_adventure,
-    tabs::tabs_adventures, version::version_update_adventures,
+    adventures::get::get_adventure,
+    app_index::index,
+    app_response::{AppError, ErrorMessage},
+    change_password::change_password,
+    change_username::change_username,
+    list::list_adventures,
+    login::login,
+    me::me,
+    play_list::play_list_adventures,
+    registry::registry,
+    sync::sync_adventure,
+    tabs::tabs_adventures,
+    version::version_update_adventures,
 };
 use axum::{
-    body::{Body, BoxBody},
-    handler::{get, post, put},
-    http::{Request, Response},
-    routing::BoxRoute,
-    AddExtensionLayer, Router,
+    body::{Body, BoxBody, Bytes},
+    error_handling::HandleErrorLayer,
+    response::IntoResponse,
+    routing::{get, post, put},
+    AddExtensionLayer, Json, Router,
 };
-use serde::ser::StdError;
-use std::convert::Infallible;
-use tower::{filter::AsyncFilterLayer, util::AndThenLayer, ServiceBuilder};
+use hyper::{Request, Response, StatusCode};
+use serde_json::json;
+use std::time::Duration;
+use tower::{filter::AsyncFilterLayer, util::AndThenLayer, BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
 
 use crate::app_state::AppState;
 
-pub fn routes(
-    state: AppState,
-) -> Router<BoxRoute<Body, Box<(dyn StdError + Send + Sync + 'static)>>> {
+pub fn routes(state: AppState) -> Router {
+    // don't change layer order, or errors happen...
     let middleware_stack = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(handle_error))
         .layer(TraceLayer::new_for_http())
         .layer(AsyncFilterLayer::new(map_request))
         .layer(AndThenLayer::new(map_response))
-        .layer(AddExtensionLayer::new(state))
-        .into_inner();
+        .timeout(Duration::from_secs(30))
+        .layer(AddExtensionLayer::new(state));
 
     let r = Router::new()
         .route("/", get(index))
@@ -46,16 +56,50 @@ pub fn routes(
         .route("/api/users/me", get(me))
         .route("/api/users/password", put(change_password))
         .route("/api/users/username", put(change_username))
-        .layer(middleware_stack)
-        .boxed();
+        .layer(middleware_stack);
 
     r
 }
 
-async fn map_request(req: Request<Body>) -> Result<Request<Body>, Infallible> {
+async fn map_request(req: Request<Body>) -> Result<Request<Body>, BoxError> {
+    let (parts, body) = req.into_parts();
+    let bytes = buffer_and_print("request", body).await?;
+    let req = Request::from_parts(parts, Body::from(bytes));
     Ok(req)
 }
 
-async fn map_response(res: Response<BoxBody>) -> Result<Response<BoxBody>, Infallible> {
+async fn map_response(res: Response<BoxBody>) -> Result<Response<Body>, BoxError> {
+    let (parts, body) = res.into_parts();
+    let bytes = buffer_and_print("response", body).await?;
+    let res = Response::from_parts(parts, Body::from(bytes));
     Ok(res)
+}
+
+async fn buffer_and_print<B>(direction: &str, body: B) -> Result<Bytes, BoxError>
+where
+    B: axum::body::HttpBody<Data = Bytes>,
+    B::Error: Into<BoxError>,
+{
+    let bytes = hyper::body::to_bytes(body).await.map_err(Into::into)?;
+    if let Ok(body) = std::str::from_utf8(&bytes) {
+       debug!("{} body = {:?}", direction, body);
+    }
+    Ok(bytes)
+}
+
+fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        Ok(StatusCode::REQUEST_TIMEOUT)
+    } else {
+        Err(AppError(
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!(ErrorMessage {
+                    message: format!("Unhandled internal error: {}", error),
+                    code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+                })),
+            )
+                .into_response(),
+        ))
+    }
 }
