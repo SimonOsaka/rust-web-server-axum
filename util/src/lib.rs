687fdf4912f6ec763b29128fcc08c758eedf25d0
Simon Osaka <geniusmickymouse@qq.com>
2022-03-18 15:35:28 +0800

    add util features, upgrade tracing

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,5 +1,9 @@
+#[cfg(any(feature = "date"))]
 pub mod date;
+#[cfg(any(feature = "excel"))]
 pub mod excel;
 
+#[cfg(any(feature = "date"))]
 pub use date::*;
+#[cfg(any(feature = "excel"))]
 pub use excel::*;
